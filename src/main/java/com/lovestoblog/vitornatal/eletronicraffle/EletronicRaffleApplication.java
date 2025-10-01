9c98d6a94aad889aa966298205cf54ccff88173d
Vitor Natal <vitornc31@gmail.com>
2025-09-30 22:01:30 -0300

    Initializing the project

---
+++ src/main/java/com/lovestoblog/vitornatal/eletronicraffle/EletronicRaffleApplication.java
@@ -0,0 +1,13 @@
+package com.lovestoblog.vitornatal.eletronicraffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicRaffleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EletronicRaffleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lovestoblog/vitornatal/eletronicraffle/EletronicRaffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovestoblog.vitornatal.eletronicraffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletronicRaffleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
