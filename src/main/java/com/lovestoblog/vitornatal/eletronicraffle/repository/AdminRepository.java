553ef2f4d43d62a727ef5b9ad76a3c06969793b8
Vitor Natal <vitornc31@gmail.com>
2025-10-06 11:36:37 -0300

    feat: creates repositories for each model

--- src/main/java/com/lovestoblog/vitornatal/eletronicraffle/repository/AdminRepository.java
+++ src/main/java/com/lovestoblog/vitornatal/eletronicraffle/repository/AdminRepository.java
@@ -3,5 +3,5 @@ package com.lovestoblog.vitornatal.eletronicraffle.repository;
 import com.lovestoblog.vitornatal.eletronicraffle.model.AdminModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminReposiroty extends JpaRepository<AdminModel, Long> {
+public interface AdminRepository extends JpaRepository<AdminModel, Long> {
 }
