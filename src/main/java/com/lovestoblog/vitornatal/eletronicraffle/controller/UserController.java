d7fc5c5708d331de1bc0413fcbf9aa7a73c42051
Vitor Natal <vitornc31@gmail.com>
2025-10-07 21:02:41 -0300

    feat: creates getUserTickets method and instantiates UserService as a dependency injection

---
+++ src/main/java/com/lovestoblog/vitornatal/eletronicraffle/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.lovestoblog.vitornatal.eletronicraffle.controller;
+
+import com.lovestoblog.vitornatal.eletronicraffle.model.RaffleTicketModel;
+import com.lovestoblog.vitornatal.eletronicraffle.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    public final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}/tickets")
+    public List<RaffleTicketModel> getUserTickets(@PathVariable Long id){
+        return userService.getUserTickets(id);
+    }
+}
