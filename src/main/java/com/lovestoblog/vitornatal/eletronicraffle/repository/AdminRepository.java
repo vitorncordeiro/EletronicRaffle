70050a32329d0adbf2beadce0a6175174da34bec
Vitor Natal <vitornc31@gmail.com>
2025-10-17 15:58:36 -0300

    refactor: Admin Entity deleted

--- src/main/java/com/lovestoblog/vitornatal/eletronicraffle/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lovestoblog.vitornatal.eletronicraffle.repository;
-
-import com.lovestoblog.vitornatal.eletronicraffle.model.AdminModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<AdminModel, Long> {
-}
