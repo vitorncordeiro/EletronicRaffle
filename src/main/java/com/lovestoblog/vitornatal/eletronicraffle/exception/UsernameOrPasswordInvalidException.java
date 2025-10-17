30c2e702d258c4aea4b5312eec424fa945040b61
Vitor Natal <vitornc31@gmail.com>
2025-10-16 21:10:45 -0300

    feat: creates new exclusive exception

---
+++ src/main/java/com/lovestoblog/vitornatal/eletronicraffle/exception/UsernameOrPasswordInvalidException.java
@@ -0,0 +1,7 @@
+package com.lovestoblog.vitornatal.eletronicraffle.exception;
+
+public class UsernameOrPasswordInvalidException extends RuntimeException{
+    public UsernameOrPasswordInvalidException(String message) {
+        super(message);
+    }
+}
