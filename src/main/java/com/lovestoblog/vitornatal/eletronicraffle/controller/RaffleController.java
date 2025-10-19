8351874c21e139c17e8edb355d64d7a2b7e2971c
Vitor Natal <vitornc31@gmail.com>
2025-10-19 17:48:34 -0300

    refactor: changes the raffle's DELETE method

--- src/main/java/com/lovestoblog/vitornatal/eletronicraffle/controller/RaffleController.java
+++ src/main/java/com/lovestoblog/vitornatal/eletronicraffle/controller/RaffleController.java
@@ -1,6 +1,7 @@
 package com.lovestoblog.vitornatal.eletronicraffle.controller;
 
 import com.lovestoblog.vitornatal.eletronicraffle.dto.request.RaffleRequestDTO;
+import com.lovestoblog.vitornatal.eletronicraffle.dto.response.RaffleResponseDTO;
 import com.lovestoblog.vitornatal.eletronicraffle.model.RaffleModel;
 import com.lovestoblog.vitornatal.eletronicraffle.service.RaffleService;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +17,13 @@ public class RaffleController {
     }
 
     @PostMapping("/createRaffle")
-    public RaffleModel createRaffle(@RequestBody RaffleRequestDTO raffleRequestDTO){
+    public RaffleResponseDTO createRaffle(@RequestBody RaffleRequestDTO raffleRequestDTO){
         return raffleService.createRaffle(raffleRequestDTO);
     }
 
     @DeleteMapping("/deleteRaffle/{id}")
-    public void deleteRaffle(@PathVariable Long id){
-        raffleService.deleteRaffle(id);
+    public void deleteRaffle(@PathVariable Long raffleId){
+        raffleService.deleteRaffle(raffleId);
     }
     @PutMapping("/editRaffle/{id}")
     public RaffleModel editRaffle(@PathVariable Long id, @RequestBody RaffleModel raffle){
--- src/main/java/com/lovestoblog/vitornatal/eletronicraffle/service/RaffleService.java
+++ src/main/java/com/lovestoblog/vitornatal/eletronicraffle/service/RaffleService.java
@@ -1,6 +1,7 @@
 package com.lovestoblog.vitornatal.eletronicraffle.service;
 
 import com.lovestoblog.vitornatal.eletronicraffle.dto.request.RaffleRequestDTO;
+import com.lovestoblog.vitornatal.eletronicraffle.dto.response.RaffleResponseDTO;
 import com.lovestoblog.vitornatal.eletronicraffle.infra.security.JWTUserData;
 import com.lovestoblog.vitornatal.eletronicraffle.mapper.RaffleMapper;
 import com.lovestoblog.vitornatal.eletronicraffle.model.RaffleModel;
@@ -8,6 +9,8 @@ import com.lovestoblog.vitornatal.eletronicraffle.repository.RaffleRepository;
 import com.lovestoblog.vitornatal.eletronicraffle.repository.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class RaffleService {
@@ -21,25 +24,29 @@ public class RaffleService {
         this.raffleRepository = raffleRepository;
     }
 
-    public RaffleModel createRaffle(RaffleRequestDTO raffleRequestDTO){
+    public RaffleResponseDTO createRaffle(RaffleRequestDTO raffleRequestDTO){
         JWTUserData principal = (JWTUserData) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         var user = userRepository.findById(principal.userId()).orElseThrow(() -> new RuntimeException("User dont found"));
-
-
         var raffleModel = raffleMapper.map(raffleRequestDTO, user);
+        raffleRepository.save(raffleModel);
 
-        return raffleRepository.save(raffleModel);
+        return raffleMapper.map(raffleModel);
     }
 
+    public void deleteRaffle(Long raffleId){
+        JWTUserData userData = (JWTUserData) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+        var raffle = raffleRepository.findById(raffleId).orElseThrow(() -> new RuntimeException("Raffle doesnt exist"));
+        if(!Objects.equals(raffle.getUser().getId(), userData.userId())){
+            throw new RuntimeException("You dont have permission to exclude this raffle");
+        }else{
+            raffleRepository.deleteById(raffleId);
+        }
 
 
 
-
-
-
-
-    public void deleteRaffle(Long id){
-        raffleRepository.deleteById(id);
     }
 
     public RaffleModel editRaffle(Long id, RaffleModel raffle){
