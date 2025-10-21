841921afbc16da594527fc9cb4afce348a422a42
Vitor Natal <vitornc31@gmail.com>
2025-10-20 21:10:52 -0300

    fix: rewrite the sql code

--- src/main/resources/db/migration/V3__WinnerTicketColumn.sql
+++ src/main/resources/db/migration/V3__WinnerTicketColumn.sql
@@ -3,4 +3,4 @@ ADD COLUMN winner_ticket_id BIGINT;
 
 ALTER TABLE raffle_model
 ADD CONSTRAINT fk_ticket_winner
-FOREIGN KEY (winner_ticket_id) REFERENCES raffle_ticket_model(id);
+FOREIGN KEY (winner_ticket_id) REFERENCES raffle_ticket_model(raffleticket_id);
