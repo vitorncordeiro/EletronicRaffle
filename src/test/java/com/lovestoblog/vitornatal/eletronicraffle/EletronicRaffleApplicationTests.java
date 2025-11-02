06b1210dd6fe674cc59755941f05bb952ebdb5a6
Vitor Natal <vitornc31@gmail.com>
2025-11-02 20:55:51 -0300

    build: changes the test application to use h2 instead of mysql

--- src/test/java/com/lovestoblog/vitornatal/eletronicraffle/EletronicRaffleApplicationTests.java
+++ src/test/java/com/lovestoblog/vitornatal/eletronicraffle/EletronicRaffleApplicationTests.java
@@ -2,10 +2,11 @@ package com.lovestoblog.vitornatal.eletronicraffle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EletronicRaffleApplicationTests {
-
     @Test
     void contextLoads() {
     }
